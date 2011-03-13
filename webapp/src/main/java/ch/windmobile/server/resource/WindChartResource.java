bc60e8ed8cab40af6e48b814498eafd7e9a9c051
Yann Savary <yann.savary@gmail.com>
2011-03-13 18:59:07 +0100

    Limit the size of the requested chart

--- webapp/src/main/java/ch/windmobile/server/resource/WindChartResource.java
+++ webapp/src/main/java/ch/windmobile/server/resource/WindChartResource.java
@@ -4,13 +4,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
 
+import ch.windmobile.server.model.DataSourceException;
 import ch.windmobile.server.model.WindMobileDataSource;
 import ch.windmobile.server.model.xml.Chart;
+import ch.windmobile.server.model.xml.Error;
 
 import com.sun.jersey.api.core.InjectParam;
 import com.sun.jersey.spi.resource.Singleton;
@@ -28,9 +33,16 @@ public class WindChartResource {
 
     @GET
     @Path("{stationId}/{duration}")
-    @Produces( { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public Chart getWindChart(@PathParam("stationId") String stationId, @PathParam("duration") int duration) {
         try {
+            // Don't kill the server, max 2 days
+            if (duration > 2 * 24 * 60 * 60) {
+                Error error = new Error();
+                error.setCode(DataSourceException.Error.SERVER_ERROR.getCode());
+                error.setMessage("Chart duration requested is too long");
+                throw new WebApplicationException(Response.status(Status.NOT_ACCEPTABLE).entity(error).build());
+            }
             return dataSource.getWindChart(stationId, duration);
         } catch (Exception e) {
             ExceptionHandler.treatException(e);
